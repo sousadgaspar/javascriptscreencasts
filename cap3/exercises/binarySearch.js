b5757101b42c858e9aa22969f6421c4864aeecbf
Sousa Gaspar <sousadgaspar@gmail.com>
2020-05-13 09:12:00 +0100

    improve the example of binary search

--- cap3/exercises/binarySearch.js
+++ cap3/exercises/binarySearch.js
@@ -1,22 +1,23 @@
 let list = [3, 6, 8, 12, 14, 17, 25, 29, 31, 36, 42, 47, 53, 55, 62];
-let begin = list[0];
-let end = list.length;
-let key = 100;
+let begin = 0;
+let end = list.length - 1;
+let searchValue = 3;
+let steps = 0;
 
-let med = (begin + end)/2;
 
 while (end >= begin) {
-    console.log("looping");
-    console.log(begin);
-    console.log(end);
-    if(med == key) {
-        console.log("found", list[med]);
-    } else if (key > med) {
+    let med = (begin + end) / 2;
+    ++steps;
+    if (searchValue == list[med]) {
+        console.log("Found", searchValue);
+    }
+
+    if (searchValue > list[med]) {
         begin = med + 1;
-    } else if (key < med) {
-        end = med - 1;
     }
     else {
-        console.log("not found", key);
+        end = med - 1;
     }
 }
+
+console.log("Total steps", steps);
