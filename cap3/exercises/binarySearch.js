9a268a5642c18608c174957b4c528283af971e36
Sousa Gaspar <sousadgaspar@gmail.com>
2020-05-13 09:21:03 +0100

    improve the algorithm to comply with binary search for odd lists

--- cap3/exercises/binarySearch.js
+++ cap3/exercises/binarySearch.js
@@ -1,12 +1,12 @@
-let list = [3, 6, 8, 12, 14, 17, 25, 29, 31, 36, 42, 47, 53, 55, 62];
+let list = [3, 6, 8, 12, 14, 17, 25, 29, 31, 36, 42, 47, 53, 55];
 let begin = 0;
 let end = list.length - 1;
-let searchValue = 3;
+let searchValue = 55;
 let steps = 0;
 
 
 while (end >= begin) {
-    let med = (begin + end) / 2;
+    let med = Math.floor((begin + end) / 2);
     ++steps;
     if (searchValue == list[med]) {
         console.log("Found", searchValue);
